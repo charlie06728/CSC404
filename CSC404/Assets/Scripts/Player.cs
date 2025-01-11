f8b5106f97da5c61285705c3c121a0cf59ee47d8
Zeyang <charlie.ni@mail.utoronto.ca>
2025-01-11 13:24:12 -0500

    collision for player

--- CSC404/Assets/Scripts/Player.cs
+++ CSC404/Assets/Scripts/Player.cs
@@ -4,9 +4,26 @@ using UnityEngine;
 namespace DefaultNamespace {
     public class Player : MonoBehaviour {
         public Transform Transform;
+        public BoxCollider Collider;
+        
+        protected bool BallWithinBounds = false;
         
         protected void Awake() {
             Transform = GetComponent<Transform>();
+            Collider = GetComponent<BoxCollider>();
+            Collider.isTrigger = false;
+        }
+
+        private void OnCollisionEnter(Collision other) {
+            BallWithinBounds = true;
+        }
+        
+        private void OnCollisionExit(Collision other) {
+            BallWithinBounds = false;
+        }
+
+        protected void Update() {
+            /* Check if the ball is  */
         }
     }
 }
