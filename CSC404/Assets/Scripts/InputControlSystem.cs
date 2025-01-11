81f974d9d144b81f6ce5efdcd413dceae2d8556c
Zeyang <charlie.ni@mail.utoronto.ca>
2025-01-11 13:13:04 -0500

    input action implementation

--- CSC404/Assets/Scripts/InputControlSystem.cs
+++ CSC404/Assets/Scripts/InputControlSystem.cs
@@ -1,7 +1,53 @@
+using System;
+using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 namespace DefaultNamespace {
     public class InputControlSystem : MonoBehaviour {
+        public InputActionMap InputActionMap;
+        public Player Player1;
+        public Player Player2;
+
+        public float PlayermoveSpeed = 1.0f;
+
+        protected bool AMovingLeft = false;
+        protected bool AMovingRight = false;
+        protected bool BMovingLeft = false;
+        protected bool BMovingRight = false;
+
+        protected void Awake() {
+            InputActionMap["AToLeft"].performed += ctx => { AMovingLeft = true; };
+            InputActionMap["AToLeft"].canceled += ctx => { AMovingLeft = false; };
+            InputActionMap["AToRight"].performed += ctx => { AMovingRight = true; };
+            InputActionMap["AToRight"].canceled += ctx => { AMovingRight = false; };
+            
+            InputActionMap["BToLeft"].performed += ctx => { BMovingLeft = true; };
+            InputActionMap["BToLeft"].canceled += ctx => { BMovingLeft = false; };
+            InputActionMap["BToRight"].performed += ctx => { BMovingRight = true; };
+            InputActionMap["BToRight"].canceled += ctx => { BMovingRight = false; };
+            
+            InputActionMap.Enable();
+        }
+
+        protected void Update() {
+            float timeDelta = Time.deltaTime;
+            if (AMovingLeft) { MovePlayerLeft(1, timeDelta); }
+            if (AMovingRight) {MovePlayerRight(1, timeDelta);}
+            if (BMovingLeft) { MovePlayerLeft(2, timeDelta); }
+            if (BMovingRight) {MovePlayerRight(2, timeDelta);}
+        }
+
+        public void MovePlayerLeft(int player, float time) {
+            Player targetPlayer = player == 1 ? Player1 : Player2;
+            Vector3 p = targetPlayer.Transform.position;
+            targetPlayer.Transform.position = new Vector3(p.x - time * PlayermoveSpeed, p.y, p.z);
+        }
         
+        public void MovePlayerRight(int player, float time) {
+            Player targetPlayer = player == 1 ? Player1 : Player2;
+            Vector3 p = targetPlayer.Transform.position;
+            targetPlayer.Transform.position = new Vector3(p.x + time * PlayermoveSpeed, p.y, p.z);
+        }
     }
 }
--- CSC404/Assets/Scripts/Player.cs
+++ CSC404/Assets/Scripts/Player.cs
@@ -1,7 +1,12 @@
+using System;
 using UnityEngine;
 
 namespace DefaultNamespace {
     public class Player : MonoBehaviour {
+        public Transform Transform;
         
+        protected void Awake() {
+            Transform = GetComponent<Transform>();
+        }
     }
 }
